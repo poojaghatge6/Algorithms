ecefeebd62a1e8e493b21d1625f9459574a00b47
Pooja Ghatge <30519010+poojaghatge6@users.noreply.github.com>
2021-08-10 21:01:20 -0500

    Create HeightOfBT.java

---
+++ HeightOfBT.java
@@ -0,0 +1,46 @@
+class TreeNode {
+  int val;
+  TreeNode right;
+  TreeNode left;
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+
+    this.val = val;
+    this.left = left;
+    this.right = right;
+
+  }
+}
+class HeightOfBT{
+  public static void main(String[] args) {
+
+    System.out.println(findTreeHeight(null)) ; 
+    System.out.println(findTreeHeight(new TreeNode(1, new TreeNode(2, null, null), new TreeNode(3, null, null)))) ; 
+    System.out.println(findTreeHeight(new TreeNode(2, 
+                                                   new TreeNode(29, 
+                                                                new TreeNode(26, null, null),
+                                                                null),
+                                                   new TreeNode(4, 
+                                                                null, 
+                                                                new TreeNode(2, 
+                                                                             new TreeNode(9, null, null),
+                                                                             null))))) ; 
+    System.out.println(findTreeHeight(new TreeNode(0 , null, null))) ; 
+
+
+  } 
+
+
+
+   public static int findTreeHeight(TreeNode root) {
+     
+     if(root == null) return 0;
+     else {
+      return Math.max(1+ findTreeHeight(root.right), 1+findTreeHeight(root.left));
+     
+     }
+     
+   }
+  
+  
+}
