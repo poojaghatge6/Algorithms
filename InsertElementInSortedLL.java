1f45138d5d449cd83b82d2b7a3cc730104e01d31
Pooja Ghatge <30519010+poojaghatge6@users.noreply.github.com>
2021-08-19 23:12:52 -0500

    Update InsertElementInSortedLL.java

--- InsertElementInSortedLL.java
+++ InsertElementInSortedLL.java
@@ -6,72 +6,52 @@ class ListNode {
   int val;
   ListNode next;
   ListNode( int val, ListNode next) {
-  
     this.next = next;
     this.val=val;
   }
 }
+
 class Solution{
   public static void main(String[] args) {
-
-
-    
       ListNode LL1 = new ListNode(1, new ListNode(3, new ListNode(4, null)));
       ListNode LL2 = new ListNode(-3, new ListNode(-2, new ListNode(-1, null)));
       display(insert(LL1, 2));
       display(insert(LL2, -4));
       display(insert(LL2, 0));
       display(insert(null, 1));
-
-
-
   } 
-  
-  
+
   public static void display(ListNode head) {
-    
     ListNode curr=head;
     while(curr!= null) {
-      
       System.out.print(curr.val+" ");
       curr = curr.next;
     }
     System.out.println();
-    
   }
   
   public static ListNode insert(ListNode head, int target) {
     
-    if(head == null) return new ListNode(target, null);
-    
+    ListNode l1 = new ListNode(target, null);
+
+    if(head == null) return l1;
+
     ListNode curr = head, prev=null;
     
     while(curr!=null) {
-      
       if(curr.val > target) {
-        ListNode temp = new ListNode(target, null);
-        if(prev!=null) prev.next = temp;
+        if(prev!=null) prev.next = l1;
         else {
-          temp.next = curr;
-          return temp;
+          head = l1;
         }
-        temp.next = curr;
-        
+        l1.next = curr;
         return head;
-        
       }
       prev = curr;
       curr = curr.next;
-      
-      
     }
-    
-    ListNode temp = new ListNode(target, null);
-    prev.next = temp;
-  
+    prev.next = l1;
     return head;
   }
-  
-
   
 }
