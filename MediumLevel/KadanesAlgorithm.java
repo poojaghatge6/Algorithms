eab80f0bee0056077be1bb2905b2e4690b193e29
Pooja Ghatge <30519010+poojaghatge6@users.noreply.github.com>
2021-08-28 15:29:08 -0500

    Update KadanesAlgorithm.java

--- MediumLevel/KadanesAlgorithm.java
+++ MediumLevel/KadanesAlgorithm.java
@@ -3,18 +3,18 @@ import java.util.*;
 class Program {
   public static int kadanesAlgorithm(int[] array) {
 		
-    // Write your code here.
-    int maxEndingHere = array[0];
-		int max = maxEndingHere ;
-		
-		for(int i=1; i<array.length; i++) {
-			maxEndingHere = Math.max(array[i], array[i] + maxEndingHere);
-			if(maxEndingHere > max) {
-				max = maxEndingHere;
-			}
+    	// Write your code here.
+    	int maxEndingHere = array[0];
+	int max = maxEndingHere ;
+
+	for(int i=1; i<array.length; i++) {
+		maxEndingHere = Math.max(array[i], array[i] + maxEndingHere);
+		if(maxEndingHere > max) {
+			max = maxEndingHere;
 		}
-		
-		
-		return max;
+	}
+
+
+	return max;
   }
 }
