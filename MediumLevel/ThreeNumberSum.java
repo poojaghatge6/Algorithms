2e878c431797ab0bc5cf0113148bf56e7eefa4c3
Pooja Ghatge <30519010+poojaghatge6@users.noreply.github.com>
2021-08-28 15:30:55 -0500

    Create ThreeNumberSum.java

---
+++ MediumLevel/ThreeNumberSum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Program {
+  public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
+    // Write your code here.
+		Arrays.sort(array);
+		ArrayList<Integer[]> list = new ArrayList<Integer[]>();
+		for(int i=0;i<array.length-2;i++) {
+			int left = i+1, right = array.length-1, sum = targetSum - array[i];
+			while(left<right) {
+				
+				if(array[left] + array[right] > sum) {
+					right--;
+				} else if(array[left] + array[right] < sum) {
+					left++;
+				} else {
+					list.add(new Integer[]{array[i], array[left], array[right] });
+					left++;
+					right--;
+				}
+				
+			}
+		}
+    return list;
+  }
+}
