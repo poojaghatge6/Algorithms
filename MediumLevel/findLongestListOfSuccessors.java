a13235aebb26faeb64a7f1022bac7e479b2ffb2e
Pooja Ghatge <30519010+poojaghatge6@users.noreply.github.com>
2021-12-04 13:07:32 -0600

    Update findLongestListOfSuccessors.java

--- MediumLevel/findLongestListOfSuccessors.java
+++ MediumLevel/findLongestListOfSuccessors.java
@@ -1,3 +1,159 @@
+/*
+ * You are given an array of words where each word consists of lowercase English letters.
+
+wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere in wordA without changing the order of the other characters to make it equal to wordB.
+
+bank = ["a","b","ba","bca","bda","bdca"] ==> 4
+["a", "ba", "bda", "bdca"]
+
+
+["a"]  => ["a"] ==> 1
+
+["", "a","b"] => ["", "a"]
+
+bank = ["ba", "a","b", "bca","bda","bdca"] 
+
+
+
+ 0  1.  2.   3.   4.    5
+
+ [ "a","b", "ba", "ca", "bca", ]
+ 
+ 
+O(n^n)
+
+O(n*m)
+       
+ 
+word = "a"
+
+curr_index = 1
+
+
+a
+
+   ba  
+   
+       bca = []
+         
+          []
+       ["bca"]
+   
+   ["ba", "bca"]
+   
+   ca
+   
+      bca = []
+      
+      
+      ["bca"
+      
+    ["ca", "bca"]
+    
+    
+["a", "ba", "bca"], [ "a" "ca", "bca"]
+
+
+   
+     
+
+
+
+
+   
+     
+   List<word>  findSuccesor (word, bank, current_index) {  a 
+   
+       List<List<word>> ans  = List
+       int cur_length = word.length;
+       
+       
+       
+       int i = current_index;
+       
+       while(bank[i].length == cur_length +1) {
+       
+          List<words> sucessors = findSuccesor( bank[i], bank, i+1)
+      }
+       
+       
+       
+       
+       
+       
+       
+      
+   
+   }
+ 
+ 
+ 
+ 
+ 
+ 
+  xbxax => "bca"
+  
+ "a"  ->  "ba" =>{ a : ["a", "ba", "bca"], []},
+ 
+ 
+ "b" -> "ba"   =>  b :  ["b", "ba", "bca"], []}
+ 
+ 
+  
+  { a : ["a", "ba", "bca"],
+ 
+ 
+    b :  ["b", "ba", "bca"]
+    
+    
+    }
+
+
+ [ "a","b", "ba", "bd", "bca"]
+
+
+
+ [ "a","b", "ba", "bd"]
+ 
+ 
+ { a : ["a", { "ba", ca } ],
+ 
+ 
+    b :  ["b", "ba"]
+    
+    
+  }
+  
+  
+  
+  
+  
+
+
+smallest_length = 1;
+
+dict = { "a" : ["a"] }
+
+
+  for (word in i) {
+     
+     if word is of length smallest 
+        dict.add{ word, [word] }
+        
+        
+     for
+        
+      
+     
+  }
+
+   isPredecessor()
+
+
+ */
+
+
+
 import java.io.*;
 import java.util.*;
 
